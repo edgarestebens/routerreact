31e59862a8a4f00caee773a31d01b49702a81d31
edgarestebens <colachoedgar@hotmail.com>
2019-01-13 10:17:08 -0500

    creando un template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { Router, Route, browserHistory} from 'react-router';
+import { Router, IndexRoute, Route, browserHistory} from 'react-router';
 import './App.css';
 
 
 const Home = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
 
+const Template =(props) => {
+  return(
+      <div>
+        <h1>Mi super aplicación</h1>
+          {props.children}
+      </div>       
+    )
+}
 
 class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-        <Route path="/" component={Home} />
-        <Route path="about"component={About} />
+        <Route path="/" component={Template}>
+          <IndexRoute component={Home} />
+          <Route path="about"component={About} />
+        </Route>
       </Router>
     );
   }
